3e83e94d2aaf09a17eca69e5cc4a1214f72b99d2
Madhuri Ghattamaneni <madhurig9999@gmail.com>
2024-05-05 15:00:18 -0700

    dfgch

--- frontend/src/Components/Navigationbar.js
+++ frontend/src/Components/Navigationbar.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
+import './Navbar.css'; 
 
 function Header() {
     const { guserID, setguserID } = useContext(AuthContext);

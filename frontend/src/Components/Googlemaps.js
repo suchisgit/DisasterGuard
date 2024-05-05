6b40ebf0f7efa28d0f3e818f0626360f656286ac
yogeshluckshetty <yogesh.luckshetty@gmail.com>
2024-05-04 18:32:05 -0700

    adf

--- frontend/src/Components/Googlemaps.js
+++ frontend/src/Components/Googlemaps.js
@@ -7,6 +7,7 @@ const sanJose = { lat: 37.335480, lng: -121.893028 };
 
 function Googlemaps() {
 
+
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     })
@@ -14,10 +15,25 @@ function Googlemaps() {
     if(!isLoaded){
         return <h1> Map is loading</h1>
     }
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+
+    //             const response = await axios.post('YOUR_ENDPOINT', { /* Your data here */ });
+
+    //             setData(response.data);
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     };
+    //     fetchData();
+    // }, []); // Empty dependency array ensures that this effect runs only once after the initial render
+
     return (
         <div className='full_width_height' >
             <GoogleMap center={sanJose} zoom={15} mapContainerStyle={{width: '100%', height: '100%'}}>
-            <Marker position={sanJose}/>
+            <Marker key={sanJose} position={sanJose}/>
             </GoogleMap>
         </div>
 

2265dd36f2cebcabe9737af38e9b0347647ce912
yogeshluckshetty <yogesh.luckshetty@gmail.com>
2024-05-04 19:26:01 -0700

    adsfasdf

--- frontend/src/Components/Googlemaps.js
+++ frontend/src/Components/Googlemaps.js
@@ -1,39 +1,42 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
 import App from '../App';
+import configData from '../config.json';
+import axios from 'axios';
 
 
 const sanJose = { lat: 37.335480, lng: -121.893028 };
 
 function Googlemaps() {
 
+    const [markers, setMarkers] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(API + 'allIncidents');
+                setMarkers(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    })
+    });
+    const API = configData.API;
 
     if(!isLoaded){
         return <h1> Map is loading</h1>
     }
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-
-    //             const response = await axios.post('YOUR_ENDPOINT', { /* Your data here */ });
-
-    //             setData(response.data);
-    //         } catch (error) {
-    //             console.error('Error fetching data:', error);
-    //         }
-    //     };
-    //     fetchData();
-    // }, []); // Empty dependency array ensures that this effect runs only once after the initial render
-
     return (
         <div className='full_width_height' >
             <GoogleMap center={sanJose} zoom={15} mapContainerStyle={{width: '100%', height: '100%'}}>
-            <Marker key={sanJose} position={sanJose}/>
+            {markers.map((marker, index) => (
+                <Marker key={index} position={{ lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) }} />
+            ))}
             </GoogleMap>
         </div>
 

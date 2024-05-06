34ca2ed4bbcb7de364dc946b7d6c6b062d229f13
yogeshluckshetty <yogesh.luckshetty@gmail.com>
2024-05-05 23:51:33 -0700

    adfasdf

--- frontend/src/Components/Googlemaps.js
+++ frontend/src/Components/Googlemaps.js
@@ -1,12 +1,24 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
+import { useJsApiLoader, GoogleMap, Marker, Circle, MarkerClusterer } from '@react-google-maps/api';
 import App from '../App';
 import configData from '../config.json';
 import axios from 'axios';
 import { AuthContext } from '../context/AuthProvider';
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
 
 const sanJose = { lat: 37.335480, lng: -121.893028 };
+const circleOptions = {
+    strokeOpacity: 0.5,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    zIndex: 3,
+    fillOpacity: 0.05,
+    strokeColor: "#FF5252",
+    fillColor: "#FF5252"
+};
 
 function Googlemaps() {
     const { role, setrole } = useContext(AuthContext);
@@ -14,12 +26,11 @@ function Googlemaps() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (role == '') {
-            navigate('/');
-          }
-          else if (role == 'user') {
-            navigate('/updateuser');
-          }
+        // if (role === '') {
+        //     navigate('/');
+        // } else if (role === 'user') {
+        //     navigate('/updateuser');
+        // }
         const fetchData = async () => {
             try {
                 const response = await axios.get(API + 'allIncidentLocations');
@@ -31,24 +42,31 @@ function Googlemaps() {
         fetchData();
     }, []);
 
-    const {isLoaded} = useJsApiLoader({
+    const { isLoaded } = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     });
     const API = configData.API;
 
-    if(!isLoaded){
+    if (!isLoaded) {
         return <h1> Map is loading</h1>
     }
 
     return (
-        <div className='full_width_height' >
-            <GoogleMap center={sanJose} zoom={15} mapContainerStyle={{width: '100%', height: '100%'}}>
-            {markers.map((marker, index) => (
-                <Marker key={index} position={{ lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) }} />
-            ))}
+        <div className='full_width_height'>
+            <GoogleMap center={sanJose} zoom={15} mapContainerStyle={{ width: '100%', height: '100%' }}>
+                {markers.map((marker, index) => (
+                    <React.Fragment key={index}>
+                        <Marker position={{ lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) }} />
+                        {/* Add Circle around each marker */}
+                        <Circle
+                            center={{ lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) }}
+                            radius={1000} // Specify the radius in meters
+                            options={circleOptions}
+                        />
+                    </React.Fragment>
+                ))}
             </GoogleMap>
         </div>
-
     )
 }
 
